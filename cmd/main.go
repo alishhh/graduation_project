04d766784b66850f8cd75cb612a8592130757742
alishhh <alisherkuttybai@gmail.com>
2022-04-11 03:32:27 +0600

    Внедрение зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 
 	darigerkz "github.com/alishhh/graduation_project"
-	handler "github.com/alishhh/graduation_project/pkg/handlers"
+	"github.com/alishhh/graduation_project/pkg/handler"
+	"github.com/alishhh/graduation_project/pkg/repository"
+	"github.com/alishhh/graduation_project/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(darigerkz.Server)
 	if err := srv.Run("8080", handlers.InitRouters()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())

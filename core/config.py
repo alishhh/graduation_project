24f29801dfb9df538c7cf03557fcf9988b750a1a
bernarww <99427366+bernarww@users.noreply.github.com>
2022-06-08 02:15:02 +0600

    Update config.py

--- core/config.py
+++ core/config.py
@@ -13,8 +13,8 @@ class Settings:
     POSTGRES_USER : str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","db_course")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","postgres")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     SECRET_KEY : str = os.getenv("SECRET_KEY")

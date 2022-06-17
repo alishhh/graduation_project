b3b7dc7c287114cc30486cc2654fc95179fa74ab
bernar <bernarww@gmail.com>
2022-06-17 07:53:40 +0600

    last fight 3

--- db/session.py
+++ db/session.py
@@ -5,22 +5,6 @@ from core.config import settings
 from typing import Generator
 
 
-# if settings.USE_SQLITE_DB == "True":
-# 	SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# 	engine = create_engine(
-# 		SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# 	)
-# else:
-# 	SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-# 	engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-
-
-
-
-# if you don't want to install postgres or any database, use sqlite, a file system based database,
-# uncomment below lines if you would like to use sqlite and comment above 2 lines of SQLALCHEMY_DATABASE_URL AND engine
-
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
